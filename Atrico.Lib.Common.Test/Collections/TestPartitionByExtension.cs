d29b91b1cc93f89a3693bcc9b3685d69a1dfce87
AtricoSoftware <developer@atrico.net>
2015-05-11 18:26:28 +0100

    IEnumerable PartitionBy added

---
+++ Atrico.Lib.Common.Test/Collections/TestPartitionByExtension.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Atrico.Lib.Assertions;
+using Atrico.Lib.Assertions.Constraints;
+using Atrico.Lib.Assertions.Elements;
+using Atrico.Lib.Common.Collections;
+using Atrico.Lib.Testing.NUnitAttributes;
+
+namespace Atrico.Lib.Common.Test.Collections
+{
+    [TestFixture]
+    public class TestPartitionByExtension
+    {
+        [Test]
+        public void TestPartitioning()
+        {
+            // Arrange
+            var list = new[]
+            {
+                1,
+                2,
+                3, 5,
+                6, 8,
+                9,
+                10
+            };
+
+            // Act
+            var result = list.PartitionBy(item => (item % 2) == 0).ToArray(); // IsEven
+
+            // Assert
+            Assert.That(Value.Of(result).Count().Is().EqualTo(6), "Number of entries");
+            Assert.That(Value.Of(result[0].Key).Is().False(), "0 = Odd");
+            Assert.That(Value.Of(result[0]).Is().EqualTo(new[] {1}), "0 Items");
+            Assert.That(Value.Of(result[1].Key).Is().True(), "1 = Even");
+            Assert.That(Value.Of(result[1]).Is().EqualTo(new[] {2}), "1 Items");
+            Assert.That(Value.Of(result[2].Key).Is().False(), "2 = Odd");
+            Assert.That(Value.Of(result[2]).Is().EqualTo(new[] {3, 5}), "2 Items");
+            Assert.That(Value.Of(result[3].Key).Is().True(), "3 = Even");
+            Assert.That(Value.Of(result[3]).Is().EqualTo(new[] {6, 8}), "3 Items");
+            Assert.That(Value.Of(result[4].Key).Is().False(), "4 = Odd");
+            Assert.That(Value.Of(result[4]).Is().EqualTo(new[] {9}), "4 Items");
+            Assert.That(Value.Of(result[5].Key).Is().True(), "5 = Even");
+            Assert.That(Value.Of(result[5]).Is().EqualTo(new[] {10}), "5 Items");
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            // Arrange
+            var list = new int[] {};
+
+            // Act
+            var result = list.PartitionBy(item => Assert.Fail("Action called"));
+
+            // Assert
+            Assert.That(Value.Of(result).Count().Is().EqualTo(0));
+        }
+    }
+}
--- Atrico.Lib.Common/Collections/EnumerableExtensions.cs
+++ Atrico.Lib.Common/Collections/EnumerableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Atrico.Lib.Common.Collections
 {
@@ -12,6 +14,50 @@ namespace Atrico.Lib.Common.Collections
                 action(item);
             }
             return list;
+        }
+
+        public static IEnumerable<IGrouping<TKey, TSource>> PartitionBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var result = new List<IGrouping<TKey, TSource>>();
+            Grouping<TKey, TSource> current = null;
+            foreach (var item in source)
+            {
+                var key = keySelector(item);
+                if (current == null || !current.Key.Equals(key))
+                {
+                    current = new Grouping<TKey, TSource>(key);
+                    result.Add(current);
+                }
+                current.Add(item);
+            }
+            return result;
+        }
+
+        private class Grouping<TKey, TSource> : IGrouping<TKey, TSource>
+        {
+            private readonly IList<TSource> _items = new List<TSource>();
+
+            public Grouping(TKey key)
+            {
+                Key = key;
+            }
+
+            public TKey Key { get; private set; }
+
+            public void Add(TSource item)
+            {
+                _items.Add(item);
+            }
+
+            public IEnumerator<TSource> GetEnumerator()
+            {
+                return _items.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable) _items).GetEnumerator();
+            }
         }
     }
 }
