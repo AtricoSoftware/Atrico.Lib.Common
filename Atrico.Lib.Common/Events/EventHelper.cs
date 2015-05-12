9ec281c2898d286ff419d1293d95593a6a2dc57a
AtricoSoftware <developer@atrico.net>
2015-05-12 18:02:40 +0100

    EventHelper added

---
+++ Atrico.Lib.Common/Events/EventHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Atrico.Lib.Common.Events
+{
+    public static class EventHelper
+    {
+        public static void Raise(this EventHandler @event, object sender)
+        {
+            var localEvent = @event;
+            if (!ReferenceEquals(localEvent, null))
+            {
+                localEvent(sender, new EventArgs());
+            }
+        }
+
+        public static void Raise(this EventHandler @event, object sender, Thread thread)
+        {
+            var localEvent = @event;
+            if (!ReferenceEquals(localEvent, null))
+            {
+                localEvent(sender, new EventArgs());
+            }
+        }
+
+        public static void Raise<TParameter1>(this EventHandler<TParameter1> @event, object sender, TParameter1 parameter1)
+        {
+            var localEvent = @event;
+            if (!ReferenceEquals(localEvent, null))
+            {
+                localEvent(sender, parameter1);
+            }
+        }
+    }
+}
