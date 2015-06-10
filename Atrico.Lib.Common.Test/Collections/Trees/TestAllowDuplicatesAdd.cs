55584a4ceab46f9a40a2d99c954a148411c118a5
Rob Rimmer <developer@atrico.net>
2015-06-10 17:20:38 +0100

    More unit tests for tree

--- Atrico.Lib.Common.Test/Collections/Trees/TestAllowDuplicatesAdd.cs
+++ Atrico.Lib.Common.Test/Collections/Trees/TestAllowDuplicatesAdd.cs
@@ -1,4 +1,5 @@
-﻿using Atrico.Lib.Assertions;
+﻿using System.Linq;
+using Atrico.Lib.Assertions;
 using Atrico.Lib.Assertions.Constraints;
 using Atrico.Lib.Assertions.Elements;
 using Atrico.Lib.Common.Collections.Tree;
@@ -27,10 +28,10 @@ namespace Atrico.Lib.Common.Test.Collections.Trees
         public void TestAddSingle()
         {
             // Arrange
-            var tree = Tree<string>.Create(true);
+            var tree = Tree<int>.Create(true);
 
             // Act
-            var node = tree.Add("node");
+            var node = tree.Add(1);
 
             // Assert
             Display(tree);
@@ -39,6 +40,62 @@ namespace Atrico.Lib.Common.Test.Collections.Trees
             Assert.That(Value.Of(tree.Children).Is().EqualTo(new[] {node}), "Tree children");
             Assert.That(Value.Of(node.Parent).Is().ReferenceEqualTo(tree), "Node parent");
             Assert.That(Value.Of(node.Children).Count().Is().EqualTo(0), "Node children count");
+        }
+
+        [Test]
+        public void TestAddMultipleUnique()
+        {
+            // Arrange
+            var tree = Tree<int>.Create(true);
+
+            // Act
+            var node1 = tree.Add(1);
+            var node11 = node1.Add(11);
+            var node12 = node1.Add(12);
+            var node13 = node1.Add(13);
+            var node121 = node12.Add(121);
+            var node122 = node12.Add(122);
+
+            // Assert
+            Display(tree);
+            var nodes = tree.GetNodes().ToArray();
+            Assert.That(Value.Of(nodes).Count().Is().EqualTo(6), "Node count");
+            Assert.That(Value.Of(nodes[0]).Is().EqualTo(new[] {1, 11}), "Node 11");
+            Assert.That(Value.Of(nodes[1]).Is().EqualTo(new[] {1, 12, 121}), "Node 121");
+            Assert.That(Value.Of(nodes[2]).Is().EqualTo(new[] {1, 12, 122}), "Node 122");
+            Assert.That(Value.Of(nodes[3]).Is().EqualTo(new[] {1, 12}), "Node 12");
+            Assert.That(Value.Of(nodes[4]).Is().EqualTo(new[] {1, 13}), "Node 13");
+            Assert.That(Value.Of(nodes[5]).Is().EqualTo(new[] {1}), "Node 1");
+        }
+
+        [Test]
+        public void TestAddMultipleDuplicate()
+        {
+            // Arrange
+            var tree = Tree<int>.Create(true);
+
+            // Act
+            var node1 = tree.Add(1);
+            var node1a = tree.Add(1);
+            var node11 = node1.Add(11);
+            var node12 = node1.Add(12);
+            var node13 = node1.Add(13);
+            var node13a = node1.Add(13);
+            var node121 = node12.Add(121);
+            var node122 = node12.Add(122);
+
+            // Assert
+            Display(tree);
+            var nodes = tree.GetNodes().ToArray();
+            Assert.That(Value.Of(nodes).Count().Is().EqualTo(8), "Node count");
+            Assert.That(Value.Of(nodes[0]).Is().EqualTo(new[] {1, 11}), "Node 11");
+            Assert.That(Value.Of(nodes[1]).Is().EqualTo(new[] {1, 12, 121}), "Node 121");
+            Assert.That(Value.Of(nodes[2]).Is().EqualTo(new[] {1, 12, 122}), "Node 122");
+            Assert.That(Value.Of(nodes[3]).Is().EqualTo(new[] {1, 12}), "Node 12");
+            Assert.That(Value.Of(nodes[4]).Is().EqualTo(new[] {1, 13}), "Node 13");
+            Assert.That(Value.Of(nodes[5]).Is().EqualTo(new[] {1, 13}), "Node 13a");
+            Assert.That(Value.Of(nodes[6]).Is().EqualTo(new[] {1}), "Node 1");
+            Assert.That(Value.Of(nodes[7]).Is().EqualTo(new[] {1}), "Node 1a");
         }
     }
 }
--- Atrico.Lib.Common/Collections/Tree/Tree.cs
+++ Atrico.Lib.Common/Collections/Tree/Tree.cs
@@ -1,16 +1,71 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Atrico.Lib.Common.Collections.Tree
 {
     /// <summary>
-    ///     Tree of nodes
+    ///     Tree utilities
+    /// </summary>
+    public static class Tree
+    {
+        /// <summary>
+        ///     Determines whether this instance is root
+        /// </summary>
+        /// <typeparam name="T">Type of node data</typeparam>
+        /// <param name="node">The node.</param>
+        /// <returns>True if node is root of tree</returns>
+        public static bool IsRoot<T>(this Tree<T>.INode node)
+        {
+            return node.Parent == null;
+        }
+
+        /// <summary>
+        ///     Get all the nodes as paths
+        /// </summary>
+        /// <typeparam name="T">Type of node data</typeparam>
+        /// <param name="node">The root of tree</param>
+        /// <returns>List of paths (list of data)</returns>
+        public static IEnumerable<IEnumerable<T>> GetNodes<T>(this Tree<T>.INode node)
+        {
+            return GetNodesImpl(node, true);
+        }
+
+        /// <summary>
+        ///     Get all the leaves (terminal nodes) as paths
+        /// </summary>
+        /// <typeparam name="T">Type of node data</typeparam>
+        /// <param name="node">The root of tree</param>
+        /// <returns>List of paths (list of data)</returns>
+        public static IEnumerable<IEnumerable<T>> GetLeaves<T>(this Tree<T>.INode node)
+        {
+            return GetNodesImpl(node, false);
+        }
+
+        private static IEnumerable<IEnumerable<T>> GetNodesImpl<T>(Tree<T>.INode node, bool includeNonTerminal)
+        {
+            var leaves = new List<IEnumerable<T>>();
+            foreach (var childLeaves in node.Children.Select(child => GetNodesImpl(child, includeNonTerminal)))
+            {
+                leaves.AddRange(childLeaves.Select(leaf => node.IsRoot() ? leaf : new[] {node.Data}.Concat(leaf)));
+            }
+            if (!node.IsRoot() && (includeNonTerminal || !leaves.Any())) leaves.Add(new[] {node.Data});
+            return leaves;
+        }
+    }
+
+    /// <summary>
+    ///     Typed tree utilities
     /// </summary>
     public static partial class Tree<T>
     {
-         public static INode Create(bool allowDuplicateNodes)
+        /// <summary>
+        ///     Creates the tree
+        /// </summary>
+        /// <param name="allowDuplicateNodes">if set to <c>true</c> allow duplicate nodes</param>
+        /// <returns>Rootnode of new tree</returns>
+        public static INode Create(bool allowDuplicateNodes)
         {
             return new Node(allowDuplicateNodes);
         }
-
     }
 }
