83a3879e1332f5b6ace3ccac3d1e5366c8d568c8
Rob Rimmer <developer@atrico.net>
2015-06-16 16:57:49 +0100

    missed files

---
+++ Atrico.Lib.Common/RegEx/Elements/RegExComposite.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Atrico.Lib.Common.Collections;
+using Atrico.Lib.Common.Collections.Tree;
+
+namespace Atrico.Lib.Common.RegEx.Elements
+{
+    public abstract partial class RegExElement
+    {
+        private abstract class RegExComposite : RegExElement
+        {
+            protected readonly IEnumerable<RegExElement> Elements;
+
+            protected RegExComposite(IEnumerable<RegExElement> elements)
+            {
+                Elements = elements;
+            }
+
+            protected static RegExElement Create(IEnumerable<RegExElement> elements, Func<IEnumerable<RegExElement>, RegExElement> creator)
+            {
+                if (elements == null) return null;
+                var elementsA = elements as RegExElement[] ?? elements.ToArray();
+                var nonNull = elementsA.Where(el => el != null).ToArray();
+                return nonNull.Count() > 1 ? creator(elementsA) : nonNull.FirstOrDefault();
+            }
+
+            protected void AddNodeToTree(Tree<string>.IModifiableNode root, string nodeName)
+            {
+                var thisNode = root.Add(nodeName);
+                foreach (var element in Elements)
+                {
+                    element.AddNodeToTree(thisNode);
+                }
+            }
+
+            protected IEnumerable<RegExElement> SimplifyChildren()
+            {
+                // Simplify children
+                return Elements.Select(el => el.Simplify());
+            }
+
+            protected static IEnumerable<RegExElement> MergeComposites<T>(IEnumerable<RegExElement> originalElements) where T : RegExComposite
+            {
+                var elements = new Stack<RegExElement>(originalElements.Reverse());
+                var newElements = new List<RegExElement>();
+                while (elements.Any())
+                {
+                    var element = elements.Pop();
+                    var elAlt = element as T;
+                    if (ReferenceEquals(elAlt, null)) newElements.Add(element);
+                    else elAlt.Elements.Reverse().ForEach(elements.Push);
+                }
+                return newElements;
+            }
+
+            #region Comparison
+
+            protected override int GetHashCodeImpl()
+            {
+                return Elements.Aggregate(0, (current, child) => current ^ child.GetHashCode());
+            }
+
+            protected override bool EqualsImpl(RegExElement obj)
+            {
+                var other = obj as RegExComposite;
+                return !ReferenceEquals(other, null) && GetType() == other.GetType() && Elements.SequenceEqual(other.Elements);
+            }
+
+            protected override int CompareToImpl(RegExElement obj)
+            {
+                var other = obj as RegExComposite;
+                // Other types always smaller, otherwise Compare character set as string
+                return !ReferenceEquals(other, null) && GetType() == other.GetType() ? Elements.SequenceCompare(other.Elements) : 1;
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ Atrico.Lib.Common/RegEx/NumberMatcher.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atrico.Lib.Common.Collections.Tree;
+using Atrico.Lib.Common.RegEx.Elements;
+using Atrico.Lib.Common.ResettableCache;
+
+namespace Atrico.Lib.Common.RegEx
+{
+    /// <summary>
+    ///     Useful reg ex constants
+    /// </summary>
+    public static class RegExHelpers
+    {
+        public class NumberMatcher
+        {
+            private readonly Tree<char>.IModifiableNode _tree = Tree<char>.Create(false);
+            private readonly ResettableCache<RegExElement> _element;
+            private readonly ResettableCache<string> _regex;
+
+            // TODO - parameterisable
+            private const bool _wholeString = true;
+
+            public NumberMatcher()
+            {
+                _regex = new ResettableCache<string>(()=>_element.Value.ToString());
+                _element = new ResettableCache<RegExElement>(()=>RegExElement.Create(_tree).Simplify(), _regex);
+            }
+
+            public NumberMatcher AddRange(uint from, uint to)
+            {
+                for (var i = from; i <= to; ++i)
+                {
+                    var digits = GetDigits(i);
+                    _tree.Add(digits);
+                }
+                _element.Reset();
+                return this;
+            }
+
+             private static IEnumerable<char> GetDigits(uint u)
+            {
+                return u.ToString("D").ToCharArray().ToList();
+            }
+
+            public override string ToString()
+            {
+                var regex = new StringBuilder(_regex.Value);
+                if (_wholeString)
+                {
+                    regex.Insert(0, '^');
+                    regex.Append('$');
+                }
+                return regex.ToString();
+            }
+
+            internal Tree<char>.INode CharacterTree { get { return _tree; } }
+            internal RegExElement Element { get { return _element.Value; } }
+        }
+    }
+}
