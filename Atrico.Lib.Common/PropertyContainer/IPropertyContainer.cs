8fa317d767a5e82154a657e2b921e3408c490c75
Rob Rimmer <developer@atrico.net>
2015-09-13 17:46:07 +0100

    Update nuspec PropertyContainer

--- Atrico.Lib.Common/PropertyContainer/IPropertyContainer.cs
+++ Atrico.Lib.Common/PropertyContainer/IPropertyContainer.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Atrico.Lib.Common.PropertyContainer
 {
-    public interface IPropertyContainer : INotifyPropertyChanged
+    public interface IPropertyContainer : INotifyPropertyChanged, IEquatable<IPropertyContainer>
     {
         T Get<T>([CallerMemberName] string name = null);
         void Set<T>(T value, [CallerMemberName] string name = null);
--- Atrico.Lib.Common/PropertyContainer/PropertyContainerBase.cs
+++ Atrico.Lib.Common/PropertyContainer/PropertyContainerBase.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Atrico.Lib.Common.PropertyContainer
 {
-    public abstract class PropertyContainerBase<TProp> : IPropertyContainer
+    public abstract class PropertyContainerBase<TProp> :  IPropertyContainer
     {
         private readonly object _owner;
         protected readonly IDictionary<string, TProp> Properties = new Dictionary<string, TProp>();
@@ -30,6 +31,7 @@ namespace Atrico.Lib.Common.PropertyContainer
             OnPropertyChanged(name);
         }
 
+ 
         protected abstract TProp CreateInitialValue<T>();
         protected abstract T GetValue<T>(string name);
         protected abstract void SetValue<T>(string name, T value);
@@ -40,5 +42,31 @@ namespace Atrico.Lib.Common.PropertyContainer
             var handler = PropertyChanged;
             if (handler != null) handler(_owner, new PropertyChangedEventArgs(propertyName));
         }
-    }
+       #region Equality
+       public override bool Equals(object obj)
+        {
+            return Equals(obj as IPropertyContainer);
+        }
+
+        public override int GetHashCode()
+        {
+            // TODO - Default values not yet requested
+            var hashcode = 17;
+            foreach (var entry in Properties.OrderBy(e => e.Key))
+            {
+                hashcode = (hashcode * 31) ^ entry.Key.GetHashCode();
+                hashcode = (hashcode * 31) ^ entry.Value.GetHashCode();
+            }
+            return hashcode;
+        }
+
+        public virtual bool Equals(IPropertyContainer other)
+        {
+            // TODO - Default values not yet requested
+            return !ReferenceEquals(other, null) && GetType() == other.GetType() && false;
+        }
+
+
+          #endregion
+     }
 }
