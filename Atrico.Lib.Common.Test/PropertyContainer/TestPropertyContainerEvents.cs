df0880576d3a6e5846c8288087381ca9b2ac8604
Rob Rimmer <developer@atrico.net>
2015-09-14 11:44:13 +0100

    Added tests for PropertyContainer

---
+++ Atrico.Lib.Common.Test/PropertyContainer/TestPropertyContainerEvents.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Atrico.Lib.Assertions;
+using Atrico.Lib.Assertions.Constraints;
+using Atrico.Lib.Assertions.Elements;
+using Atrico.Lib.Testing;
+using Atrico.Lib.Testing.TestAttributes.NUnit;
+
+namespace Atrico.Lib.Common.Test.PropertyContainer
+{
+    [TestFixture]
+    public class TestPropertyContainerEvents : TestFixtureBase
+    {
+        private class PropertyHandler
+        {
+            public int CallCount { get; private set; }
+            public PropertyChangedEventArgs Args { get; private set; }
+
+            public void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                Args = e;
+                ++CallCount;
+            }
+        }
+
+        private static string GetCallingMethodName([CallerMemberName] string name = null)
+        {
+            return name;
+        }
+
+        [Test]
+        public void TestSetDefault()
+        {
+            var name = GetCallingMethodName();
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(this);
+            var handler = new PropertyHandler();
+            props.PropertyChanged += handler.OnPropertyChanged;
+
+            // Act
+            props.Set(RandomValues.Integer());
+            props.PropertyChanged -= handler.OnPropertyChanged;
+
+            // Assert
+            Assert.That(Value.Of(handler.CallCount).Is().EqualTo(1), "Single callback");
+            Assert.That(Value.Of(handler.Args.PropertyName).Is().EqualTo(name), "Correct name");
+        }
+
+        [Test]
+        public void TestSetMultiple()
+        {
+            var name = GetCallingMethodName();
+            const int count = 10;
+            var values = RandomValues.UniqueValues<int>(count);
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(this);
+            var handler = new PropertyHandler();
+            props.PropertyChanged += handler.OnPropertyChanged;
+
+            // Act
+            foreach (var val in values)
+            {
+                props.Set(val);
+            }
+            props.PropertyChanged -= handler.OnPropertyChanged;
+
+            // Assert
+            Assert.That(Value.Of(handler.CallCount).Is().EqualTo(count), "Multiple callbacks");
+            Assert.That(Value.Of(handler.Args.PropertyName).Is().EqualTo(name), "Correct name");
+        }
+
+        [Test]
+        public void TestSetNoChange()
+        {
+            var name = GetCallingMethodName();
+            var values = Enumerable.Repeat(123, 10);
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(this);
+            var handler = new PropertyHandler();
+            props.PropertyChanged += handler.OnPropertyChanged;
+
+            // Act
+            foreach (var val in values)
+            {
+                props.Set(val);
+            }
+            props.PropertyChanged -= handler.OnPropertyChanged;
+
+            // Assert
+            Assert.That(Value.Of(handler.CallCount).Is().EqualTo(1), "Single callback");
+            Assert.That(Value.Of(handler.Args.PropertyName).Is().EqualTo(name), "Correct name");
+        }
+    }
+}
---
+++ Atrico.Lib.Common.Test/PropertyContainer/TestPropertyContainerGetSet.cs
@@ -0,0 +1,70 @@
+﻿using Atrico.Lib.Assertions;
+using Atrico.Lib.Assertions.Constraints;
+using Atrico.Lib.Assertions.Elements;
+using Atrico.Lib.Testing;
+using Atrico.Lib.Testing.TestAttributes.NUnit;
+
+namespace Atrico.Lib.Common.Test.PropertyContainer
+{
+    [TestFixture]
+    public class TestPropertyContainerGetSet<T> : TestPODTypes<T>
+    {
+        [Test]
+        public void TestGetDefault()
+        {
+            var name = RandomValues.String();
+            var expected = default(T);
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(null);
+
+            // Act
+            var value = props.Get<T>(name);
+
+            // Assert
+            if (typeof(T) != typeof(string))
+            {
+                Assert.That(Value.Of(value).Is().EqualTo(expected), "Get default value");
+            }
+            else
+            {
+                Assert.That(Value.Of(value as string).Is().Null(), "Get default value (string)");
+            }
+        }
+
+        [Test]
+        public void TestSetAndGet()
+        {
+            var name = RandomValues.String();
+            var expected = RandomValues.Value<T>();
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(null);
+            props.Set(expected, name);
+
+            // Act
+            var value = props.Get<T>(name);
+
+            // Assert
+            Assert.That(Value.Of(value).Is().EqualTo(expected), "Get value");
+        }
+
+        [Test]
+        public void TestOverwriteAndGet()
+        {
+            var name = RandomValues.String();
+            var expected = RandomValues.Value<T>();
+
+            // Arrange
+            var props = new Common.PropertyContainer.PropertyContainer(null);
+            props.Set(RandomValues.Value<T>(), name);
+            props.Set(expected, name);
+
+            // Act
+            var value = props.Get<T>(name);
+
+            // Assert
+            Assert.That(Value.Of(value).Is().EqualTo(expected), "Get value");
+        }
+    }
+}
