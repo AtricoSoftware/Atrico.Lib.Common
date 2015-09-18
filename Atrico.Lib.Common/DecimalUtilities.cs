7e76b0a0d9b13a0bd5578de283a5d4ccf4e68a1a
Rob Rimmer <developer@atrico.net>
2015-09-18 16:37:09 +0100

    Decimal unitilies added

---
+++ Atrico.Lib.Common/DecimalUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Atrico.Lib.Common
+{
+    /// <summary>
+    /// Utilities for decimal values
+    /// </summary>
+    public static class DecimalUtilities
+    {
+        /// <summary>
+        /// Trims trailing zeros in decimal value
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>String representation of value</returns>
+        public static string ToTrimmedString(this decimal value)
+        {
+            var str = value.ToString(CultureInfo.InvariantCulture);
+            if (!str.Contains('.'))
+            {
+                return str;
+            }
+            str = str.TrimEnd('0');
+            str = str.TrimEnd('.');
+            return str;
+        }
+    }
+}
