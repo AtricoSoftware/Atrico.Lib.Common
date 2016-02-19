bb8a34f549d8da7920a90c39e0edd3fa72580a5a
RobRimmer <robr@mrlender.co.uk>
2016-02-19 14:29:42 +0000

    Set date time added

---
+++ Atrico.Lib.Common/DateAndTime/ISetDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atrico.Lib.Common.DateAndTime
+{
+	/// <summary>
+	/// Interface for setting the date/time
+	/// </summary>
+	public interface ISetDateTime
+	{
+		/// <summary>
+		/// Set the current date
+		/// Stored as offset so 24 hours after setting date, date will have increased
+		/// </summary>
+		/// <param name="date">Date to set</param>
+		void SetDate(DateTime date);
+	}
+}
---
+++ Atrico.Lib.Common/DateAndTime/ModifiableDateTimeProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Atrico.Lib.Common.DateAndTime
+{
+	/// <summary>
+	///	Modifiable provider for date/time
+	///	The date can be set
+	/// </summary>
+	public class ModifiableDateTimeProvider : IDateTimeProvider, ISetDateTime
+	{
+		private long _daysOffset = 0L;
+
+		public DateTime Now
+		{
+			get { return DateTime.Now.AddDays(_daysOffset); }
+		}
+
+		public DateTimeOffset NowOffset
+		{
+			get { return DateTimeOffset.Now.AddDays(_daysOffset); }
+		}
+
+		public void SetDate(DateTime date)
+		{
+			_daysOffset = (long)((date.Date - DateTime.Now.Date).TotalDays);
+		}
+	}
+}
